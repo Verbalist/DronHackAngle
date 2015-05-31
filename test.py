e0c89c823bfcfc1e9a3654752e3cb5d76a86e4c2
JobyKK <jobyand121@gmail.com>
2015-05-31 08:08:32 +0300

    sqr added

--- test.py
+++ test.py
@@ -13,16 +13,18 @@ def img_process(frame):
 
 	main_rect = (0, 0)
 
+	sum_sqr_rect = 0
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
 		main_rect = (main_rect[0] + x + w/2, main_rect[1] + y + h/2)
+		sum_sqr_rect += w*h
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 	main_rect = (main_rect[0]/len(faces),main_rect[1]/len(faces))
 
 	w, h = frame.shape[:2]
 
-	return main_rect[0]-h/2, main_rect[1]-w/2
+	return main_rect[0]-h/2, main_rect[1]-w/2, sum_sqr_rect
 	# Display the resulting frame
 	#cv2.imwrite('test.jpeg', frame)
 
